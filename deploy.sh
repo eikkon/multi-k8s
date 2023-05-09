8e2493edbad3e3a88c24e21e98079f9d51832f42
eikkon <eikkon@DESKTOP-ECIJ9TR.localdomain>
2023-05-09 19:39:58 +0200

    added deployment for google cloud IX on change dedocker build

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
-docker build -t eikkon/multi-client:latest -t eikkon/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t eikkon/multi-server:latest -t eikkon/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t eikkon/multi-worker:latest -t eikkon/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push eikkon/multi-client:latest
-docker push eikkon/multi-server:latest
-docker push eikkon/multi-worker:latest
-docker push eikkon/multi-client:$SHA
-docker push eikkon/multi-server:$SHA
-docker push eikkon/multi-worker:$SHA
+docker build -t cygnetops/multi-client-k8s:latest -t cygnetops/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t cygnetops/multi-server-k8s-pgfix:latest -t cygnetops/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t cygnetops/multi-worker-k8s:latest -t cygnetops/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+
+docker push cygnetops/multi-client-k8s:latest
+docker push cygnetops/multi-server-k8s-pgfix:latest
+docker push cygnetops/multi-worker-k8s:latest
+
+docker push cygnetops/multi-client-k8s:$SHA
+docker push cygnetops/multi-server-k8s-pgfix:$SHA
+docker push cygnetops/multi-worker-k8s:$SHA
+
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=eikkon/multi-server:$SHA 
-kubectl set image deployments/client-deployment client=eikkon/multi-client:$SHA 
-kubectl set image deployments/worker-deployment worker=eikkon/multi-worker:$SHA 
+kubectl set image deployments/server-deployment server=cygnetops/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=cygnetops/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=cygnetops/multi-worker-k8s:$SHA
